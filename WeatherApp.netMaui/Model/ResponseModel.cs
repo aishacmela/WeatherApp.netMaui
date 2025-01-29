c4eab125774f4725bf970647e55953153bc1a86b
Aishacmela <aishacmela@gmail.com>
2025-01-29 14:16:23 +0200

    updated the ui

--- WeatherApp.netMaui/Model/ResponseModel.cs
+++ WeatherApp.netMaui/Model/ResponseModel.cs
@@ -9,9 +9,6 @@ namespace WeatherApp.netMaui.Model
 {
 	public class ResponseModel
 	{
-		[JsonPropertyName("request")]
-		public ResponseModelRequest request { get; set; }
-
 		[JsonPropertyName("location")]
 		public ResponseModelLocation location { get; set; }
 
@@ -19,45 +16,24 @@ namespace WeatherApp.netMaui.Model
 		public ResponseModelCurrent current { get; set; }
 	}
 
-	public class ResponseModelRequest
-	{
-		public string type { get; set; }
-		public string query { get; set; }
-		public string language { get; set; }
-		public string unit { get; set; }
-	}
 
 	public class ResponseModelLocation
 	{
 		public string name { get; set; }
 		public string country { get; set; }
-		public string region { get; set; }
-		public string lat { get; set; }
-		public string lon { get; set; }
-		public string timezone_id { get; set; }
-		public string localtime { get; set; }
-		public int localtime_epoch { get; set; }
-		public string utc_offset { get; set; }
 	}
 
 	public class ResponseModelCurrent
 	{
 		public string observation_time { get; set; }
 		public int temperature { get; set; }
-		public int weather_code { get; set; }
 		public string[] weather_icons { get; set; }
 		public string[] weather_descriptions { get; set; }
 		public int wind_speed { get; set; }
-		public int wind_degree { get; set; }
-		public string wind_dir { get; set; }
-		public int pressure { get; set; }
-		public int precip { get; set; }
 		public int humidity { get; set; }
-		public int cloudcover { get; set; }
 		public int feelslike { get; set; }
 		public int uv_index { get; set; }
-		public int visibility { get; set; }
-		public string is_day { get; set; }
+		
 	}
 
 
